ac446b63f98b5b61bcb0c198c2020387a4c6475e
SJLuisGabriel <19031333@itcelaya.edu.mx>
2024-05-17 13:10:30 -0600

    feat: Agregar vista para el usaurio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TEDW_ecomerce_front';
+<<<<<<< Updated upstream
+=======
+  showFooter = true;
+  showNavUsers = true;
+  showNavNotUsers = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(
+          (event: Event): event is NavigationEnd =>
+            event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event: NavigationEnd) => {
+        // Lógica para showFooter
+        this.showFooter = !event.url.includes('/login');
+
+        // Lógica para showNavUsers
+        this.showNavUsers =
+          event.url.includes('/user/home') ||
+          event.url.includes('/user/shop') ||
+          event.url.includes('/user/services') ||
+          event.url.includes('/user/about') ||
+          event.url.includes('/user/contact') ||
+          event.url.includes('/user/cart');
+
+        this.showNavNotUsers = !this.showNavUsers;
+      });
+  }
+>>>>>>> Stashed changes
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,17 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
+<<<<<<< Updated upstream
+=======
+import { LoginComponent } from './components/login/login.component';
+import { IndexComponent } from './components/usuario/index/index.component';
+import { NavUsuarioComponent } from './components/usuario/nav-usuario/nav-usuario.component';
+import { CartComponent } from './components/usuario/cart/cart.component';
+import { ShopUserComponent } from './components/usuario/shop-user/shop-user.component';
+import { ServicesUserComponent } from './components/usuario/services-user/services-user.component';
+import { ContactUserComponent } from './components/usuario/contact-user/contact-user.component';
+import { AboutUserComponent } from './components/usuario/about-user/about-user.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -21,6 +32,7 @@ import { FooterComponent } from './components/footer/footer.component';
     HomeComponent,
     ContactComponent,
     AboutComponent,
+<<<<<<< Updated upstream
     FooterComponent
   ],
   imports: [
@@ -33,6 +45,36 @@ import { FooterComponent } from './components/footer/footer.component';
       {path: 'about', component:AboutComponent},
       {path: '**', redirectTo:'/', pathMatch: 'full'}
     ])
+=======
+    FooterComponent,
+    LoginComponent,
+    IndexComponent,
+    NavUsuarioComponent,
+    CartComponent,
+    ShopUserComponent,
+    ServicesUserComponent,
+    ContactUserComponent,
+    AboutUserComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      //Aquí definimos las rutas con base a los componentes que se vayan creando
+      { path: 'home', component: HomeComponent },
+      { path: 'shop', component: ShopComponent },
+      { path: 'services', component: ServicesComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'user/home', component: IndexComponent },
+      { path: 'user/cart', component: CartComponent },
+      { path: 'user/shop', component: ShopUserComponent },
+      { path: 'user/services', component: ServicesUserComponent },
+      { path: 'user/contact', component: ContactUserComponent },
+      { path: 'user/about', component: AboutUserComponent },
+      { path: '**', redirectTo: '/home', pathMatch: 'full' },
+    ]),
+>>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
