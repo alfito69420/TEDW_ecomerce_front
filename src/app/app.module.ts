c348cbea520cdce61bd10fe42dc3a3f9b0e842ac
SJLuisGabriel <19031333@itcelaya.edu.mx>
2024-05-18 17:27:46 -0600

    fix: Restructuración del app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,24 +14,7 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< Updated upstream
-=======
-import { LoginComponent } from './components/login/login.component';
-import { IndexComponent } from './components/usuario/index/index.component';
-import { NavUsuarioComponent } from './components/usuario/nav-usuario/nav-usuario.component';
-import { CartComponent } from './components/usuario/cart/cart.component';
-import { ShopUserComponent } from './components/usuario/shop-user/shop-user.component';
-import { ServicesUserComponent } from './components/usuario/services-user/services-user.component';
-import { ContactUserComponent } from './components/usuario/contact-user/contact-user.component';
-import { AboutUserComponent } from './components/usuario/about-user/about-user.component';
-import { IndexComponent } from './components/usuario/index/index.component';
-import { NavUsuarioComponent } from './components/usuario/nav-usuario/nav-usuario.component';
-import { CartComponent } from './components/usuario/cart/cart.component';
-import { ShopUserComponent } from './components/usuario/shop-user/shop-user.component';
-import { ServicesUserComponent } from './components/usuario/services-user/services-user.component';
-import { ContactUserComponent } from './components/usuario/contact-user/contact-user.component';
-import { AboutUserComponent } from './components/usuario/about-user/about-user.component';
->>>>>>> Stashed changes
+// import { LoginComponent } from './components/login/login.component';
 import { IndexComponent } from './components/usuario/index/index.component';
 import { NavUsuarioComponent } from './components/usuario/nav-usuario/nav-usuario.component';
 import { CartComponent } from './components/usuario/cart/cart.component';
@@ -46,43 +32,7 @@ import { AboutUserComponent } from './components/usuario/about-user/about-user.c
     HomeComponent,
     ContactComponent,
     AboutComponent,
-<<<<<<< Updated upstream
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([ //Aquí definimos las rutas con base a los componentes que se vayan creando
-      {path: '', component:HomeComponent},
-      {path: 'shop', component:ShopComponent},
-      {path: 'services', component:ServicesComponent},
-      {path: 'contact', component:ContactComponent},
-      {path: 'about', component:AboutComponent},
-      {path: '**', redirectTo:'/', pathMatch: 'full'}
-    ])
-=======
     FooterComponent,
-    LoginComponent,
-    IndexComponent,
-    NavUsuarioComponent,
-    CartComponent,
-    ShopUserComponent,
-    ServicesUserComponent,
-    ContactUserComponent,
-    AboutUserComponent,
-    IndexComponent,
-    NavUsuarioComponent,
-    CartComponent,
-    ShopUserComponent,
-    ServicesUserComponent,
-    ContactUserComponent,
-    AboutUserComponent,
-    IndexComponent,
-    NavUsuarioComponent,
-    CartComponent,
-    ShopUserComponent,
-    ServicesUserComponent,
-    ContactUserComponent,
-    AboutUserComponent,
     IndexComponent,
     NavUsuarioComponent,
     CartComponent,
@@ -95,14 +45,12 @@ import { AboutUserComponent } from './components/usuario/about-user/about-user.c
     BrowserModule,
     RouterModule.forRoot([
       //Aquí definimos las rutas con base a los componentes que se vayan creando
-      { path: 'home', component: HomeComponent },
-      { path: 'home', component: HomeComponent },
-      { path: 'home', component: HomeComponent },
-      { path: 'home', component: HomeComponent },
+      { path: '', component: HomeComponent },
       { path: 'shop', component: ShopComponent },
       { path: 'services', component: ServicesComponent },
       { path: 'contact', component: ContactComponent },
       { path: 'about', component: AboutComponent },
+      { path: '**', redirectTo: '/', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'user/home', component: IndexComponent },
       { path: 'user/cart', component: CartComponent },
@@ -110,32 +58,9 @@ import { AboutUserComponent } from './components/usuario/about-user/about-user.c
       { path: 'user/services', component: ServicesUserComponent },
       { path: 'user/contact', component: ContactUserComponent },
       { path: 'user/about', component: AboutUserComponent },
-      { path: '**', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'user/home', component: IndexComponent },
-      { path: 'user/cart', component: CartComponent },
-      { path: 'user/shop', component: ShopUserComponent },
-      { path: 'user/services', component: ServicesUserComponent },
-      { path: 'user/contact', component: ContactUserComponent },
-      { path: 'user/about', component: AboutUserComponent },
-      { path: '**', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'user/home', component: IndexComponent },
-      { path: 'user/cart', component: CartComponent },
-      { path: 'user/shop', component: ShopUserComponent },
-      { path: 'user/services', component: ServicesUserComponent },
-      { path: 'user/contact', component: ContactUserComponent },
-      { path: 'user/about', component: AboutUserComponent },
-      { path: '**', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'user/home', component: IndexComponent },
-      { path: 'user/cart', component: CartComponent },
-      { path: 'user/shop', component: ShopUserComponent },
-      { path: 'user/services', component: ServicesUserComponent },
-      { path: 'user/contact', component: ContactUserComponent },
-      { path: 'user/about', component: AboutUserComponent },
-      { path: '**', redirectTo: '/home', pathMatch: 'full' },
     ]),
->>>>>>> Stashed changes
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
