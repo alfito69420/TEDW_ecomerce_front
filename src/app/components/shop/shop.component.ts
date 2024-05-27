ce2b067cff4b778623825af9c8d840d91345fd8f
Ricardo Miguel Garcia Noguez <ricardo.garcia.noguez.1220@gmail.com>
2024-05-27 14:22:57 -0600

    feat: consumo productos

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuariosServiceService } from '../../services/usuarios-service.service';
+import { ActivatedRoute } from '@angular/router';
+declare const noUiSlider: any;
+declare const $: any;
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  public productos: any[] = [];
+
+  constructor(private _usuarioService: UsuariosServiceService) {
+
+  }
+
+  ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this._usuarioService.vista_productos('').subscribe(
+      data => {
+        this.productos = data;
+      },
+      error => {
+        console.error('Error al obtener los productos', error);
+      }
+    );
+  }
 
 }
